013e26d1aba78f2674115a4abcf7d88271dd2597
khaiphong <duong.batien@gmail.com>
2025-06-21 21:11:26 -0600

    rust Agent

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,35 @@
 // rated dimensions of the persona (ydimension, xdimension, fdimension, x_traits, y_pointers) 
 
-struct Persona {
-    active: bool,
-    name: String,
+struct User {
     email: String,
+    name: String,
+    
+    active: bool,
     sign_in_count: u64,
 }
 
 fn main() {
-  
-  let user_template = Persona {
-  	active: true,
-    name: String::from("someusername123"),
-    email: String::from("someone@example.com"),
-    sign_in_count: 1,
-    
-  };
 
-  let me = Persona {
-   	name: String::from("Duong BaTien"),
-    email: String::from("duong.batien@gmail.com"),
-    ..user_template
-  };
-   
+  let email = "duong.batien@gmail.com".to_string();
+  let name = "Duong BaTien".to_string();
+  
+  let me = build_user(email, name);
+
+  println!("My email is {}", me.email);   
   println!("My name is {}", me.name);
-  println!("My new email is {}", me.email);
+
   println!("Am I active: {}", me.active);
   println!("My sign_in_count {}", me.sign_in_count);
   
+}
+
+fn build_user(email: String, name: String) -> User {
+  User {
+    name, email,
+    active: true,
+    sign_in_count: 1,
+    
+  }
 }
 
 
