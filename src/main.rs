f18d3223f3fa485bb50dfcff55e1d403541e0f21
khaiphong <duong.batien@gmail.com>
2025-06-21 20:31:03 -0600

    rust Agent

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+// rated dimensions of the persona (ydimension, xdimension, fdimension, x_traits, y_pointers) 
+
+struct Persona {
+    active: bool,
+    name: String,
+    email: String,
+    sign_in_count: u64,
 }
+
+fn main() {
+  
+  let user_template = Persona {
+  	active: true,
+    name: String::from("someusername123"),
+    email: String::from("someone@example.com"),
+    sign_in_count: 1,
+    
+  };
+
+  let me = Persona {
+   	name: String::from("Duong BaTien"),
+    email: String::from("duong.batien@gmail.com"),
+    ..user_template
+  };
+   
+  println!("My name is {}", me.name);
+  println!("My new email is {}", me.email);
+  println!("Am I active: {}", me.active);
+  println!("My sign_in_count {}", me.sign_in_count);
+  
+}
+
+
---
+++ target/debug/deps/mu-b30b616077ba53cd.d
@@ -0,0 +1,5 @@
+/home/batien/Kp/GitSrc/github.com/khaiphong/mu/target/debug/deps/mu-b30b616077ba53cd: src/main.rs
+
+/home/batien/Kp/GitSrc/github.com/khaiphong/mu/target/debug/deps/mu-b30b616077ba53cd.d: src/main.rs
+
+src/main.rs:
